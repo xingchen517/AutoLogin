ee01ccfc8731d0f28a67267742e539cbd5eab2ec
fulton <642791865@163.com>
2017-08-20 11:04:00 +0800

    添加项目文件。

---
+++ AutoLogin/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using AutoLogin.Model;
+
+namespace AutoLogin
+{
+    [Activity(Label = "AutoLogin", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private EditText txtUserName = null;
+        private EditText txtPwd = null;
+        private CheckBox ckbxRemPwd = null;
+        private CheckBox ckbxAutologin = null;
+        private Button btnLogin = null;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+            txtUserName = FindViewById<EditText>(Resource.Id.txtUsrName);
+            txtPwd = FindViewById<EditText>(Resource.Id.txtPwd);
+            ckbxRemPwd = FindViewById<CheckBox>(Resource.Id.ckBxRemPwd);
+            ckbxAutologin = FindViewById<CheckBox>(Resource.Id.ckBxAutoLogin);
+
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            btnLogin.Click += BtnLogin_Click;
+        }
+
+        private void BtnLogin_Click(object sender, System.EventArgs e)
+        {
+            var usrName = txtUserName.Text;
+            var pwd = txtPwd.Text;
+            if (new Login().Execute(usrName, pwd))
+            {
+                btnLogin.Text = "登录成功！";
+            }
+            else
+            {
+                btnLogin.Text = "失败，请重试！";
+            }
+        }
+    }
+}
+
---
+++ AutoLogin/Model/Login.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AutoLogin.Model
+{
+    public class Login
+    {
+
+
+        HttpWebRequest webRequest2 = (HttpWebRequest)WebRequest.Create(new Uri("http://www.xxxx.com/chk.asp"));
+
+
+        public bool Execute(string user,string pwd)
+        {
+            string loginUrl = "http://202.114.74.218/web3/login_g.aspx";
+            string loginData = "uid=******&pwd=******";
+            CookieContainer cookies = new CookieContainer();
+            string loginResult = HttpPost(loginUrl, loginData, cookies);
+            Console.WriteLine("这是登陆后的界面信息！");
+            Console.WriteLine(loginResult);
+
+            HttpWebResponse response2 = (HttpWebResponse)webRequest2.GetResponse();
+            StreamReader sr2 = new StreamReader(response2.GetResponseStream(), Encoding.Default);
+            string text2 = sr2.ReadToEnd();
+            return true;
+        }
+        public static string HttpPost(string url, string data, CookieContainer cookies)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            //FORM元素的enctype属性指定了表单数据向服务器提交时所采用的编码类型，默认的缺省值是“application/x-www-form-urlencoded”  
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = Encoding.UTF8.GetByteCount(data);
+            request.CookieContainer = cookies;
+            Stream requetStream = request.GetRequestStream();
+            StreamWriter streamWriter = new StreamWriter(requetStream);
+            streamWriter.Write(data);
+            streamWriter.Close();
+
+            request.CookieContainer = cookies;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            response.Cookies = cookies.GetCookies(response.ResponseUri);
+            cookies.Add(response.Cookies);
+            Stream responseStream = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(responseStream, Encoding.GetEncoding("GB2312"));
+            string result = streamReader.ReadToEnd();
+            streamReader.Close();
+            responseStream.Close();
+            return result;
+        }
+    }
+}
